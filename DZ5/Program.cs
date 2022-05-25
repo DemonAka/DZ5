84d045bb66bd01fe4350454a867121099d91a4a6
d_akulich@outlook.com <d_akulich@outlook.com>
2022-05-25 23:15:23 +0300

    Добавьте файлы проекта.

---
+++ DZ5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DZ5
+{
+    internal class Program
+    {
+
+
+        static string[] SeparatWords(string phrase)
+        {
+            string[] masWord = phrase.Split(' ');
+            Print(masWord);
+            return masWord;
+        }
+
+        static string[] Print(string[] masWord)
+        {
+            foreach (var word in masWord)
+            {
+                Console.WriteLine($"{word}");
+            }
+            return masWord;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение или бессвязный набор букв, а я разделю их на слова");
+            SeparatWords(Console.ReadLine());
+            Console.ReadKey();
+        }
+
+    }
+}
